240d32da6a8d2c8941e5b11eb84a22abd670f2df
XiaoyuLi <xiaoyu.li@shanbay.com>
2017-04-07 17:36:35 +0800

    Initial Commit

---
+++ Photos/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Photos
+//
+//  Created by Xiaoyu Li on 07/04/2017.
+//  Copyright © 2017 com.shanbay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
